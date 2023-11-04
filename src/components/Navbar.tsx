7aeac77447c90ff9896ae300dab8748bd02be6d6
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-04 21:44:24 +0100

    added navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import links from '@/data/data'
+import links from "@/data/data"
 
 const Navbar = () => {
 
---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+const links = [
+    {
+        id: 1,
+        title: "HOME",
+        url: "/"
+    },
+    {
+        id: 2,
+        title: "ABOUT US",
+        url: "/about"
+    },
+    {
+        id: 3,
+        title: "ACCOMODATION",
+        url: "/accomodation"
+    },
+    {
+        id: 4,
+        title: "GALLERY",
+        url: "/gallery"
+    },
+    {
+        id: 5,
+        title: "CONTACT",
+        url: "/contact"
+    }
+    
+]
+
+export default links
