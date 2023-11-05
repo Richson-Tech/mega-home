4bb781d02ea1dd75c4a43463491267549226702a
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-05 22:28:37 +0100

    new commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,14 +7,14 @@ const Navbar = () => {
   return (
     <div className=' flex justify-between h-20 bg-white text text-black'>
      <Link href="/" className='flex items-center p-4 text-4xl font-bold font-[AutourOne] hover:text-[#AA825D]'>
-     Groove
+     LOGO
      </Link>
      <div className='flex items-center justify-end gap-10 ml-32 text-base font-medium'>
     {links.map(({id,url,title}) => 
     <Link key={id} className='hover:text-[#9B804E]' href={url}>{title}</Link>
     )}
      </div>
-    <div className='flex items-center p-4'>
+    <div className='flex items-center justify-center p-2'>
     <span className="h-10 w-40 text-black hover:text-orange-300">JOIN US</span>
     <span className="h-10 w-40 text-black hover:text-orange-300">ADD JOURNEY</span>
     </div>
