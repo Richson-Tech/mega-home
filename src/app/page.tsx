b6b40537d3d1f3bea85f108b71f54c60f4ca5c2d
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-05 22:35:59 +0100

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Icode
+    <main>
+      <div className='h-max'>
+<Image src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZXN0YXRlfGVufDB8fDB8fHww"
+width={450}
+height={450}/>
+      </div>
     </main>
   )
 }
